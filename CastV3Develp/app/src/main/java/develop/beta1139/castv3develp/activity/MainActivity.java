e6dcfd534883f4b27a018f40f6d1326b191c88ec
tomo1139 <tomo1139@gmail.com>
2017-02-11 12:08:41 +0900

    toolbarにcastアイコン追加

--- CastV3Develp/app/src/main/java/develop/beta1139/castv3develp/activity/MainActivity.java
+++ CastV3Develp/app/src/main/java/develop/beta1139/castv3develp/activity/MainActivity.java
@@ -2,7 +2,10 @@ package develop.beta1139.castv3develp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.google.android.gms.cast.framework.CastButtonFactory;
 import com.google.android.gms.cast.framework.CastContext;
 
 import develop.beta1139.castv3develp.R;
@@ -10,6 +13,7 @@ import develop.beta1139.castv3develp.R;
 public class MainActivity extends AppCompatActivity {
 
     private CastContext mCastContext;
+    private MenuItem mediaRouteMenuItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +21,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mCastContext = CastContext.getSharedInstance(this);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.browse, menu);
+        mediaRouteMenuItem = CastButtonFactory.setUpMediaRouteButton(getApplicationContext(), menu, R.id.media_route_menu_item);
+        return true;
     }
 }
