bb9f62a591ee8a292798bceeba16070c59a4e8a2
tomo1139 <tomo1139@gmail.com>
2017-02-11 12:06:01 +0900

    cast idk v3の取り込み

--- CastV3Develp/app/src/main/java/develop/beta1139/castv3develp/activity/MainActivity.java
+++ CastV3Develp/app/src/main/java/develop/beta1139/castv3develp/activity/MainActivity.java
@@ -1,13 +1,21 @@
-package develop.beta1139.castv3develp;
+package develop.beta1139.castv3develp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.cast.framework.CastContext;
+
+import develop.beta1139.castv3develp.R;
+
 public class MainActivity extends AppCompatActivity {
+
+    private CastContext mCastContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCastContext = CastContext.getSharedInstance(this);
     }
 }
